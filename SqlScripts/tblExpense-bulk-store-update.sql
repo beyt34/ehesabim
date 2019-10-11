2ac68aab1ca04cd333265f4ede99d9ad621fdd1e
Beytullah TAŞKIN <beytullah@tekkredi.com>
2019-10-11 16:08:34 +0300

    .dashbord html

---
+++ SqlScripts/tblExpense-bulk-store-update.sql
@@ -0,0 +1,48 @@
+﻿UPDATE tblExpense 
+	SET ExpenseStoreId='0e54c237-0aee-4ca0-8554-15e6bc92a572' -- diğer
+		,Notes='Gold, '+Notes
+WHERE ExpenseStoreId='b1f4895d-1cb7-4f57-a71f-71f9eaf5f18e' -- gold bilgisayar
+
+UPDATE tblExpense 
+	SET ExpenseStoreId='0e54c237-0aee-4ca0-8554-15e6bc92a572' -- diğer
+		,Notes='Anadolu Sigorta, '+Notes
+WHERE ExpenseStoreId='eb384144-1484-4d0c-bcb8-ffc1dfccca5a' -- anadolu sigorta
+
+UPDATE tblExpense 
+	SET ExpenseStoreId='0e54c237-0aee-4ca0-8554-15e6bc92a572' -- diğer
+		,Notes='Axa Sigorta, '+Notes
+WHERE ExpenseStoreId='643f46f2-aa3a-4e91-8748-4d1523ad5365' -- axa sigorta
+
+UPDATE tblExpense 
+	SET ExpenseStoreId='0e54c237-0aee-4ca0-8554-15e6bc92a572' -- diğer
+		,Notes='Fiba Sigorta, '+Notes
+WHERE ExpenseStoreId='a6037a97-3b08-4f3d-9f0a-315a2d52ae4e' -- fiba sigorta
+
+UPDATE tblExpense 
+	SET ExpenseStoreId='0e54c237-0aee-4ca0-8554-15e6bc92a572' -- diğer
+		,Notes='Mapfre Sigorta, '+Notes
+WHERE ExpenseStoreId='9deb8c0a-260b-4c29-93e2-9167bb461bfc' -- mapfre sigorta
+
+UPDATE tblExpense 
+	SET ExpenseStoreId='0e54c237-0aee-4ca0-8554-15e6bc92a572' -- diğer
+		,Notes='limango.com.tr, '+Notes
+WHERE ExpenseStoreId='c7fa950b-e812-4511-854f-ff1088094180' -- limango.com.tr
+
+UPDATE tblExpense 
+	SET ExpenseStoreId='0e54c237-0aee-4ca0-8554-15e6bc92a572' -- diğer
+		,Notes='markafoni.com, '+Notes
+WHERE ExpenseStoreId='40202e4b-21db-4d19-9619-ecdebf7e9d31' -- markafoni.com
+
+UPDATE tblExpense 
+	SET ExpenseStoreId='0e54c237-0aee-4ca0-8554-15e6bc92a572' -- diğer
+		,Notes='mizu.com, '+Notes
+WHERE ExpenseStoreId='a0abad12-6d51-494a-b979-bccca2191a27' -- mizu.com
+
+UPDATE tblExpense 
+	SET ExpenseStoreId='0e54c237-0aee-4ca0-8554-15e6bc92a572' -- diğer
+		,Notes='Sompo Japan, '+Notes
+WHERE ExpenseStoreId='b069ba16-ef4e-48cd-92a6-658f149f4ae5' -- Sompo Japan
+
+
+
+
